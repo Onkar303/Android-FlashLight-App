0afb9d35e2da4bbef343a51bc5ad0184d5c2a86b
Onkar303 <onkarkalpavriksha@gmail.com>
2019-06-05 17:41:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ChatHeadServcie.java
@@ -0,0 +1,234 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.PixelFormat;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import static android.content.ContentValues.TAG;
+
+public class ChatHeadServcie extends Service {
+
+    private WindowManager windowManager;
+    private View mChatHeadView;
+    boolean isFlashLightOn=false;
+
+    public ChatHeadServcie()
+    {
+
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mChatHeadView=LayoutInflater.from(this).inflate(R.layout.chatheadlayout,null);
+
+        final WindowManager.LayoutParams params=new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT
+        );
+
+
+        params.gravity= Gravity.TOP|Gravity.LEFT;
+        params.x=0;
+        params.y=100;
+
+        windowManager=(WindowManager)getSystemService(Context.WINDOW_SERVICE);
+        windowManager.addView(mChatHeadView, params);
+
+        Button fab=(Button)mChatHeadView.findViewById(R.id.fab);
+        fab.setOnTouchListener(new View.OnTouchListener() {
+
+            int lastAction;
+            int intitalX;
+            int intitalY;
+            float initialTouchX;
+            float initialTouchY;
+
+
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+
+                        intitalX=params.x;
+                        intitalY=params.y;
+
+                        initialTouchX=event.getRawX();
+                        initialTouchY=event.getRawY();
+                        return true;
+
+                    case MotionEvent.ACTION_UP:
+
+                        /*if(lastAction==MotionEvent.ACTION_DOWN)
+                        {
+                            intitalX = params.x;
+                            intitalY = params.y;
+
+                            //get the touch location
+                            initialTouchX = event.getRawX();
+                            initialTouchY = event.getRawY();
+
+                            Toast.makeText(ChatHeadServcie.this, "clicked", Toast.LENGTH_SHORT).show();
+                        }
+                        lastAction=event.getAction();*/
+
+                        if( (Math.abs(initialTouchX - event.getRawX())<5) && (Math.abs(initialTouchY - event.getRawY())<5) )
+                        {
+
+                                configureFlashLight();
+
+                        }
+                        else Log.e(TAG,"you moved the head");
+                        return true;
+
+
+
+                    case  MotionEvent.ACTION_MOVE:
+
+                        params.x = intitalX + (int) (event.getRawX() - initialTouchX);
+                        params.y = intitalY + (int) (event.getRawY() - initialTouchY);
+
+                        windowManager.updateViewLayout(mChatHeadView, params);
+                        lastAction = event.getAction();
+                        return true;
+
+                }
+
+                return false;
+
+
+
+            }
+        });
+
+
+        ImageView cancelView=(ImageView)mChatHeadView.findViewById(R.id.removeView);
+        cancelView.bringToFront();
+
+        cancelView.setOnTouchListener(new View.OnTouchListener() {
+
+            int lastAction;
+            int intitalX;
+            int intitalY;
+            float initialTouchX;
+            float initialTouchY;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch (event.getAction())
+                {
+
+
+                    case MotionEvent.ACTION_DOWN:
+
+                        intitalX=params.x;
+                        intitalY=params.y;
+
+                        initialTouchX=event.getRawX();
+                        initialTouchY=event.getRawY();
+                        return true;
+
+                    case MotionEvent.ACTION_UP:
+                        if( (Math.abs(initialTouchX - event.getRawX())<5) && (Math.abs(initialTouchY - event.getRawY())<5) )
+                        {
+                            windowManager.removeView(mChatHeadView);
+                            stopSelf();
+
+                        }
+
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+
+        //the below method is redunadant as we handle this event during the on touch listner
+
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @RequiresApi(api = Build.VERSION_CODES.M)
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if(mChatHeadView == null)
+        {
+           // windowManager.removeView(mChatHeadView);
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void configureFlashLight(){
+
+        CameraManager cameraManager=(CameraManager)getSystemService(CAMERA_SERVICE);
+
+        if(isFlashLightOn)
+        {
+            try{
+                String camerId=cameraManager.getCameraIdList()[0];
+                cameraManager.setTorchMode(camerId,false);
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+
+            isFlashLightOn=false;
+        }else
+        {
+            try{
+                String camerId=cameraManager.getCameraIdList()[0];
+                cameraManager.setTorchMode(camerId,true);
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+
+            isFlashLightOn=true;
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,318 @@
+package com.example.myapplication;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.GradientDrawable;
+import android.hardware.camera2.CameraManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    FloatingActionButton mainButton,leftOffButton,rightOnButton;
+    CameraManager camManager;
+    String cameraId = null;
+    boolean isOpen=false;
+    private static final int CODE_DRAW_OVER_OTHER_APP_PERMISSION = 2084;
+    Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setFullScreen();
+        setTranslucentNavigation();
+
+
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initChathead();
+            }
+        });
+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.CAMERA,Manifest.permission.INTERNET},
+                1);
+
+        initAdMob();
+
+        init();
+
+
+    }
+
+    public void initChathead(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+
+            //If the draw over permission is not available open the settings screen
+            //to grant the permission.
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, CODE_DRAW_OVER_OTHER_APP_PERMISSION);
+        } else {
+            initializeView();
+        }
+
+    }
+
+    private void initializeView() {
+                startService(new Intent(MainActivity.this, ChatHeadServcie.class));
+                finish();
+    }
+
+
+    public boolean hasFlashLight()
+    {
+        return this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+    }
+
+
+    public void init()
+    {
+
+        //experiment
+//
+//        GradientDrawable gradientDrawable = new GradientDrawable(
+//                GradientDrawable.Orientation.TOP_BOTTOM,
+//                new int[]{ContextCompat.getColor(this, R.color.colorAccent),
+//                        ContextCompat.getColor(this, R.color.colorPrimary),
+//                        ContextCompat.getColor(this, R.color.colorPrimaryDark),
+//                       });
+//
+//        findViewById(R.id.parent_layout).setBackground(gradientDrawable);
+
+
+        camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        mainButton=(FloatingActionButton)findViewById(R.id.mainButton);
+
+        leftOffButton=(FloatingActionButton)findViewById(R.id.leftOfButton);
+
+
+        rightOnButton=(FloatingActionButton)findViewById(R.id.rightOnButton);
+
+
+        mainButton.setOnClickListener(this);
+        leftOffButton.setOnClickListener(this);
+        rightOnButton.setOnClickListener(this);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.mainButton:
+                showButton();
+                break;
+
+            case R.id.rightOnButton:
+                startFlash();
+                break;
+
+            case R.id.leftOfButton:
+                endFlash();
+                break;
+
+        }
+    }
+
+
+    public void MyToast(String message){
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void ErrorAlert(String errorMessage)
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("Error");
+        builder.setMessage(errorMessage);
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+
+        AlertDialog dialog=builder.create();
+        dialog.show();
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void startFlash() {
+        try {
+            cameraId = camManager.getCameraIdList()[0];
+            camManager.setTorchMode(cameraId, true);
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void endFlash(){
+
+        try{
+            cameraId = camManager.getCameraIdList()[0];
+            camManager.setTorchMode(cameraId, false);
+        }catch (Exception e)
+        {
+          e.printStackTrace();
+        }
+
+    }
+
+    public void showButton()
+    {
+
+        if(!isOpen)
+        {
+            Animation rightopenanimation = AnimationUtils.loadAnimation(this,R.anim.rightfadein);
+            rightopenanimation.setDuration(100);
+
+            Animation leftopenanimation = AnimationUtils.loadAnimation(this,R.anim.leftfadein);
+            leftopenanimation.setDuration(100);
+
+            rightOnButton.startAnimation(rightopenanimation);
+            leftOffButton.startAnimation(leftopenanimation);
+
+
+            rightOnButton.setVisibility(View.VISIBLE);
+            leftOffButton.setVisibility(View.VISIBLE);
+
+            isOpen=true;
+        }
+        else
+        {
+            Animation rightcloseanimation = AnimationUtils.loadAnimation(this,R.anim.rightfadeout);
+            rightcloseanimation.setDuration(100);
+
+            Animation leftcloseanimation = AnimationUtils.loadAnimation(this,R.anim.leftfadeout);
+            leftcloseanimation.setDuration(100);
+
+            rightOnButton.startAnimation(rightcloseanimation);
+            leftOffButton.startAnimation(leftcloseanimation);
+
+            rightOnButton.setVisibility(View.GONE);
+            leftOffButton.setVisibility(View.GONE);
+            isOpen=false;
+        }
+
+
+
+    }
+
+
+    public void initAdMob(){
+        MobileAds.initialize(this,getResources().getString(R.string.admob_ID));
+        AdView mAdView = (AdView)findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+
+                    onStop();
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+    public void setFullScreen()
+    {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    public void setTranslucentNavigation()
+    {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == CODE_DRAW_OVER_OTHER_APP_PERMISSION) {
+
+            //Check if the permission is granted or not.
+            if (resultCode == RESULT_OK) {
+                initializeView();
+            } else { //Permission is not available
+                Toast.makeText(this,
+                        "Draw over other app permission not available. Closing the application",
+                        Toast.LENGTH_LONG ).show();
+                finish();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
+    public void MyAlertDialog()
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("Attention");
+        builder.setMessage("Keep flash active ?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+    }
+}
+
